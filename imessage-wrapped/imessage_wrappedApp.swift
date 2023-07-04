8fce52fa7960a601a69ec8726fab7594a552efcb
Tommy Lam <tommylam1004@gmail.com>
2023-07-04 16:51:32 -0400

    Initial Commit

---
+++ imessage-wrapped/imessage_wrappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  imessage_wrappedApp.swift
+//  imessage-wrapped
+//
+//  Created by Tommy Lam on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct imessage_wrappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
